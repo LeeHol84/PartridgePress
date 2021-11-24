e0ee26d748c4b72cfe7ed1471d59c0ec16b25485
Lee <leeholgate1@gmail.com>
2021-11-24 09:50:30 +0000

    new file

---
+++ app.js
@@ -0,0 +1,80 @@
+document.addEventListener('keydown', function(e) {
+    if (e.which == 65) {
+       document.getElementById('audio').play();
+    }
+    else if (e.which == 66) {
+        document.getElementById('audio1').play();   
+    }
+    else if (e.which == 67) {
+        document.getElementById('audio2').play();   
+    }
+    else if (e.which == 68) {
+        document.getElementById('audio3').play();   
+    }
+    else if (e.which == 69) {
+        document.getElementById('audio4').play();   
+    }
+    else if (e.which == 70) {
+        document.getElementById('audio5').play();   
+    }
+    else if (e.which == 71) {
+        document.getElementById('audio6').play();   
+    }
+    else if (e.which == 72) {
+        document.getElementById('audio7').play();   
+    }
+    else if (e.which == 73) {
+        document.getElementById('audio8').play();   
+    }
+    else if (e.which == 74) {
+        document.getElementById('audio9').play();   
+    }
+    else if (e.which == 75) {
+        document.getElementById('audio10').play();   
+    }
+    else if (e.which == 76) {
+        document.getElementById('audio11').play();   
+    }
+    else if (e.which == 77) {
+        document.getElementById('audio12').play();   
+    }
+    else if (e.which == 78) {
+        document.getElementById('audio13').play();   
+    }
+    else if (e.which == 79) {
+        document.getElementById('audio14').play();   
+    }
+    else if (e.which == 80) {
+        document.getElementById('audio15').play();   
+    }
+    else if (e.which == 81) {
+        document.getElementById('audio16').play();   
+    }
+    else if (e.which == 82) {
+        document.getElementById('audio17').play();   
+    }
+    else if (e.which == 83) {
+        document.getElementById('audio18').play();   
+    }
+    else if (e.which == 84) {
+        document.getElementById('audio19').play();   
+    }
+    else if (e.which == 85) {
+        document.getElementById('audio20').play();   
+    }
+    else if (e.which == 86) {
+        document.getElementById('audio21').play();   
+    }
+    else if (e.which == 87) {
+        document.getElementById('audio22').play();   
+    }
+    else if (e.which == 88) {
+        document.getElementById('audio23').play();   
+    }
+    else if (e.which == 89) {
+        document.getElementById('audio24').play();   
+    }
+    else if (e.which == 90) {
+        document.getElementById('audio25').play();   
+    }
+ });
